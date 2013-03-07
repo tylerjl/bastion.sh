7b839307904b8ec9cdd012872620c868bf1cf020
Tyler Langlois <tjl@byu.net>
2013-03-07 02:24:53 +0000

    Add update file

---
+++ tasks/02_updates.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Find the distro and set the appropriate package manager and update command
+if echo "${DISTRO}" | grep -E -i '(centos|rh|fc)' >/dev/null ; then
+  # Yum-based
+  PKG_MGR="$(which yum)"
+  UPDATE_CMD="${PKG_MGR} update -y"
+else
+  # DPKG based
+  PKG_MGR="$(which apt-get)"
+  UPDATE_CMD="${PKG_MGR} upgrade -y"
+fi
+
+task_precheck() {
+
+  # Ensure the package manager is present
+  if ! silence which $PKG_MGR ; then
+    echo "${PKG_MGR} does not exist"
+    return 3
+  fi
+
+  for bin in wc bc ; do
+    if ! silence which $bin ; then
+      echo "${bin} binary is missing"
+      return 4
+    fi
+  done
+
+  if [ "$(basename ${PKG_MGR})" = "yum" ] ; then
+    UPDATE_CHECK="$(${PKG_MGR} list updates -q | wc -l | xargs echo -1 + | bc)"
+  elif [ "$(basename ${PKG_MGR})" = "apt-get" ] ; then
+    silence ${PKG_MGR} update || echo "Could not update ${PKG_MGR}" ; return 5
+    UPDATE_CHECK="$(${PKG_MGR} -q --assume-no | grep installed | awk '{ print $1; }')"
+  fi
+
+  if [ ${UPDATE_CHECK} = 0 ] ; then
+    echo "No updates available"
+    return 1
+  fi
+
+  return 0
+}
+
+task_explain() {
+  if [ "$(basename ${PKG_MGR})" = "yum" ] ; then
+    UPDATE_CHECK="$(${PKG_MGR} list updates -q | wc -l | xargs echo -1 + | bc)"
+  elif [ "$(basename ${PKG_MGR})" = "apt-get" ] ; then
+    silence ${PKG_MGR} update || echo "Could not update ${PKG_MGR}" ; return 5
+    UPDATE_CHECK="$(${PKG_MGR} -q --assume-no | grep installed | awk '{ print $1; }')"
+  fi
+
+  echo "Going to install ${UPDATE_CHECK} updates with:\n\t\t\t${UPDATE_CMD}"
+}
+
+task_run() {
+  eval $UPDATE_CMD
+}
+
